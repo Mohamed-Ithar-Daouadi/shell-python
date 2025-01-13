b9092b98c695a044f4f4fb3047e6b4a81ff6b6a3
Ithar Daouadi <ithardaouadi@gmail.com>
2025-01-13 14:09:37 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-    while True:
-        command=input()
-        print(f"{command}: command not found\n")
-        main()
+    
+    command=input()
+    print(f"{command}: command not found\n")
+    main()
 
 
 if __name__ == "__main__":
