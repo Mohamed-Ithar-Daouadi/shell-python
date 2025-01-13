ced751b03e2366a324656e8506f882b11f0d72b3
Ithar Daouadi <ithardaouadi@gmail.com>
2025-01-13 14:36:35 +0300

    attempt4

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ def main():
     
     sys.stdout.write("$ ")
     command=input()
-    print(f"{command}: command not found")
+    if command == "exit 0":
+        exit()
+    else :
+        print(f"{command}: command not found")
     main()
 
 
