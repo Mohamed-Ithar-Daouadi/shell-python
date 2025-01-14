1fe6a883f925173529cf6ce6c75fb42e41c60526
Ithar Daouadi <ithardaouadi@gmail.com>
2025-01-14 13:35:56 +0300

    attempt6

--- app/main.py
+++ app/main.py
@@ -4,8 +4,15 @@ import sys
 def main():
     
     sys.stdout.write("$ ")
+    builtins=["exit", "echo", "type"]
     command=input()
+    
     match command.split():
+        case ["type {command}"]:
+            if command in builtins:
+                print(f"{command} is a shell builtin")
+            else:
+                print(f"{command} not found")
         case ["exit","0"]:
             exit()
         case ["echo", *args]:
